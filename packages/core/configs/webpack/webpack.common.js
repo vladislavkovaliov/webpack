55f74f653a7db2ebd20e7ffc568d110f994fc235
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2022-03-13 19:27:14 +0300

    chore: try vercel

--- packages/core/configs/webpack/webpack.common.js
+++ packages/core/configs/webpack/webpack.common.js
@@ -20,7 +20,7 @@ output: {
      name: "core",
      remotes: {
        "store": process.env.ENV === "production" ? "store@https://webpack-ten.vercel.app/store.js" : "store@http://localhost:3000/store.js",
-       "login": process.env.ENV === "production" ? "login@https://login-qup2qt20f-vladislavkovaliov.vercel.app/login.js" : "login@http://localhost:3002/login.js",
+       "login": process.env.ENV === "production" ? "login@https://login-ee2r9w6es-vladislavkovaliov.vercel.app/login.js" : "login@http://localhost:3002/login.js",
        "attackChains": process.env.ENV === "production" ? "attackChains@https://attack-chains.vercel.app/attackChains.js" : "attackChains@http://localhost:3003/attackChains.js",
      },
      shared: { react: { singleton: true }, 'react-dom': { singleton: true }, 'react-router-dom': { singleton: true } },
